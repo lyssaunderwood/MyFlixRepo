db570429ee079b03ad2a922bbc04c79893c4aa89
Alyssa Underwood <lyssaunderwood@fb.com>
2016-06-17 14:52:18 -0700

    Update

--- app/src/main/java/com/example/lyssaunderwood/flixster/MoviesActivity.java
+++ app/src/main/java/com/example/lyssaunderwood/flixster/MoviesActivity.java
@@ -36,6 +36,7 @@ public class MoviesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+        getSupportActionBar().hide();
 
 //        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
 //        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
